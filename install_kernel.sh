720caeb7f8855a30a601e3263a42bcfe6d0c3121
KangDroid <hyunwoo286@gmail.com>
2020-10-26 23:48:15 +0900

    Improve some scripts

--- install_kernel.sh
+++ install_kernel.sh
@@ -6,6 +6,7 @@ sudo cp ./Image /boot/
 sudo cp ./dtb/* /boot/
 
 sudo cp -vr modules_testing/lib/modules/$KERNVER /lib/modules/
+sudo cp -vr header_test/include /usr/
 
 sudo install -p -m 644 8192cu.ko /lib/modules/$KERNVER
 sudo /sbin/depmod -a $KERNVER
